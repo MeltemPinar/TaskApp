d4365302d69ab8d8299c73b3f3a3801713967b18
MeltemPinar <nemmeltem@gmail.com>
2024-08-21 21:41:18 +0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider} from '@ui-kitten/components';
+import RootNavigator from './src/router/rootnavigator';
+export default function App() {
+  return (
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </ApplicationProvider>
+  );
+}
---
+++ src/screens/addTask/index.jsx
@@ -0,0 +1,106 @@
+//import liraries
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import uuid from 'react-native-uuid';
+import {Input, Button, Radio, RadioGroup} from '@ui-kitten/components';
+import {Formik} from 'formik';
+import CustomDatePicker from '../../components/uı/customDatePicker';
+import {taskSchema} from '../../utils/validations';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {status} from '../../utils/constants';
+const AddTask = () => {
+  const saveTask = async values => {
+    try {
+      const savedTasks = await AsyncStorage.getItem('tasks');
+      let myTask = savedTasks ? JSON.parse(savedTasks) : [];
+      myTask.push(values);
+      await AsyncStorage.setItem('tasks', JSON.stringify(myTask));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Formik
+        initialValues={{
+          id: uuid.v4(),
+          description: '',
+          title: '',
+          startDate: null,
+          endDate: null,
+          category: null,
+          status: status.ONGOING,
+        }}
+        validationSchema={taskSchema}
+        onSubmit={values => saveTask(values)}>
+        {({handleChange, handleSubmit, values, setFieldValue, errors}) => (
+          <View>
+            <Input
+              size="large"
+              style={{marginVertical: 10}}
+              value={values.title}
+              label={'Title'}
+              placeholder=""
+              onChangeText={handleChange('title')}
+              status={errors.title ? 'danger' : 'basic'}
+              caption={errors.title}
+            />
+            <Input
+              multiline
+              size="large"
+              style={{marginVertical: 10}}
+              value={values.description}
+              label={'Description'}
+              placeholder=""
+              onChangeText={handleChange('description')}
+              status={errors.description ? 'danger' : 'basic'}
+              caption={errors.description}
+            />
+            <CustomDatePicker
+              size="large"
+              style={{marginVertical: 10}}
+              date={values.startDate}
+              label={'Start Date'}
+              onSelectDate={date => setFieldValue('startDate', date)}
+              status={errors.startDate ? 'danger' : 'basic'}
+              caption={errors.startDate}
+            />
+            <CustomDatePicker
+              size="large"
+              style={{marginVertical: 10}}
+              date={values.endDate}
+              label={'End Date'}
+              onSelectDate={date => setFieldValue('endDate', date)}
+              status={errors.endDate ? 'danger' : 'basic'}
+              caption={errors.endDate}
+            />
+            <RadioGroup
+              selectedIndex={values.category}
+              onChange={index => setFieldValue('category', index)}>
+              <Radio status="success">Software</Radio>
+              <Radio status="success">Design</Radio>
+              <Radio status="success">Operation</Radio>
+            </RadioGroup>
+            <Button
+              status="success"
+              style={{marginTop: 30}}
+              onPress={handleSubmit}>
+              CREATE
+            </Button>
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 15,
+  },
+});
+
+//make this component available to the app
+export default AddTask;
